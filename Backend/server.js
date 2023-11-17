dee64ddbedaaf74cc962bd4ea023d2cba5854977
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2023-11-17 21:52:35 +0530

    project completed

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(cors());
